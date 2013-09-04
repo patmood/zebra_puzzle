629e27257ce9c6a7064fca010de68f07e4a76ce5
patmood <patrick.n.moody@gmail.com>
2013-09-04 13:51:58 -0700

    first commit

---
+++ zebra.rb
@@ -0,0 +1,11 @@
+
+
+
+
+# Possibilities
+color = ["yellow","blue","red","ivory","green"]
+nation = ["norway","ukranine","england","spain","japan"]
+drink = ["water","tea","milk","oj","coffee"]
+smoke = ["kools","chesterfield","old gold","lucky strike","parliament"]
+pet = ["fox","horse","snails","dog","zebra"]
+
